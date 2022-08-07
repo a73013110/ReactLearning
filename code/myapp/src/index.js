e78f19c8d6b7f6b3dbd59298e3a54ce72f5da1e1
王義凱 <a73013110@gmail.com>
2022-08-07 15:21:36 +0800

    第一個Component

--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import App from './01-base/01-class組件'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <div><b>Hello World</b></div>
+    <App/>
 );
