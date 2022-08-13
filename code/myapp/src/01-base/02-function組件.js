45d9a7b054dd93a93e20625142db9c0ccf54f4ed
王義凱 <a73013110@gmail.com>
2022-08-13 12:14:27 +0800

    function組件

---
+++ code/myapp/src/01-base/02-function組件.js
@@ -0,0 +1,11 @@
+
+/**
+ * React <16.8: 無狀態組件
+ * React >=16.8: 透過react hooks => 狀態組件
+ * @return {*} 
+ */
+function App() {
+    return <div>Hello World Function</div>
+}
+
+export default App
--- code/myapp/src/index.js
+++ code/myapp/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './01-base/01-class組件'
+// import App from './01-base/01-class組件'
+import App from './01-base/02-function組件'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
