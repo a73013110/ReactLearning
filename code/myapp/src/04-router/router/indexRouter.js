2df17f18f23cd43d0950ddb8447dc405005331eb
Yikai <a73013110@hanglong.com.tw>
2022-08-23 18:08:22 +0800

    Route：重定向、NotFound(半成品)

--- code/myapp/src/04-router/router/indexRouter.js
+++ code/myapp/src/04-router/router/indexRouter.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Films from '../views/Films'
 import Cenimas from '../views/Cenimas'
 import Center from '../views/Center'
+import NotFound from '../views/NotFound'
 
 export default class indexRouter extends Component {
     render() {
@@ -13,6 +14,9 @@ export default class indexRouter extends Component {
                     <Route path={'/films'} element={<Films />} />
                     <Route path={'/cinemas'} element={<Cenimas />} />
                     <Route path={'/center'} element={<Center />} />
+                    
+                    <Route path={'/'} element={<Navigate to="/films" replace />} />
+                    {/* <Route element={<NotFound />} /> */}
                 </Routes>
             </HashRouter>
         )
