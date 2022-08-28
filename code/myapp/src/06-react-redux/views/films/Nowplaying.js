49a488365403ff837a5f17b6d92445e90ce5f04f
王義凱 <a73013110@gmail.com>
2022-08-28 17:25:18 +0800

    nowplaying改取本地資料，速度較快

--- code/myapp/src/06-react-redux/views/films/Nowplaying.js
+++ code/myapp/src/06-react-redux/views/films/Nowplaying.js
@@ -9,14 +9,15 @@ export default function Nowplaying(props) {
 
     useEffect(() => {
         // axios第三方庫，專門用來請求資料
-        axios({
-            // url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
-            url: "https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=994837",
-            headers: {
-                'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.1","e":"16613345111022180741611521"}',
-                'X-Host': 'mall.film-ticket.film.list'
-            }
-        }).then(res => {
+        // axios({
+        //     // url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
+        //     url: "https://m.maizuo.com/gateway?cityId=110100&pageNum=1&pageSize=10&type=1&k=994837",
+        //     headers: {
+        //         'X-Client-Info': '{"a":"3000","ch":"1002","v":"5.2.1","e":"16613345111022180741611521"}',
+        //         'X-Host': 'mall.film-ticket.film.list'
+        //     }
+        // }).then(res => {
+        axios.get("/test.json").then(res => {
             console.log(res.data.data.films)
             setList(res.data.data.films)
         }).catch(e => {
