f45f09d80652fe1c70c2679cfaea37eec9d66f2f
Yikai <a73013110@hanglong.com.tw>
2022-08-30 17:58:15 +0800

    調整redux-thunk action方法 React18以後使用dispatch Slice action不需要return (dispatch) => { ...axios語法 }，就能正常執行

--- code/myapp/src/06-react-redux/redux/actionCreator/CinemaListAction.js
+++ code/myapp/src/06-react-redux/redux/actionCreator/CinemaListAction.js
@@ -2,25 +2,24 @@ import axios from 'axios'
 
 /**
  * redux-thunk方法
+ * React18以後使用dispatch Slice action不需要return (dispatch) => { ...axios語法 }，就能正常執行
  * @returns 
  */
  export default function getCinemaListAction() {
-    return (dispatch) => {
-        axios({
-            // url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
-            url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
-            , method: "get"
-        }).then(res => {
-            // console.log(res.data.data.epg)
-            // dispatch({
-            //     type: "change-list",
-            //     payload: res.data.data.epg
-            // })
-            return res.data.data.epg
-        }).catch(e => {
-            console.log(e)
-        })
-    }
+    return axios({
+        // url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
+        url: "https://pcw-api.iq.com/api/albumList?deviceId=1bf4c1d372a30a38baa3a4e0e7bd462c&platformId=4&langCode=zh_tw&modeCode=ntw&pn=1&ps=20&chnId=2"
+        , method: "get"
+    }).then(res => {
+        // console.log(res.data.data.epg)
+        // dispatch({
+        //     type: "change-list",
+        //     payload: res.data.data.epg
+        // })
+        return res.data.data.epg
+    }).catch(e => {
+        console.log(e)
+    })
 }
 
 /**
