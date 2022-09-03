00a1c23196886724559d6e25895fcb1f1cc2c1ed
王義凱 <a73013110@gmail.com>
2022-09-03 22:41:16 +0800

    mobx-react: 完成初版

--- code/myapp/src/10-mobx/04-router/App.js
+++ code/myapp/src/10-mobx/04-router/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { HashRouter } from 'react-router-dom'
 import { autorun } from 'mobx'
+import { observer } from 'mobx-react'
 
 import Tabbar from './components/Tabbar'
 import MRouter from './router'
@@ -9,17 +10,19 @@ import store from './mobx/store'
 import './views/css/App.css'
 
 export default function App() {
-    const [isShow, setIsShow] = useState(store.isTabbarShow)
+    // const [isShow, setIsShow] = useState(store.isTabbarShow)
 
-    useEffect(() => {
-        autorun(() => {
-            setIsShow(store.isTabbarShow)
-        })
-    }, [])
+    // useEffect(() => {
+    //     autorun(() => {
+    //         setIsShow(store.isTabbarShow)
+    //     })
+    // }, [])
+
+    const TabbarObserver = observer(() => store.isTabbarShow && <Tabbar></Tabbar>)
 
     return (
         <HashRouter>
-            {isShow && <Tabbar></Tabbar>}
+            <TabbarObserver></TabbarObserver>
             <MRouter></MRouter>
         </HashRouter>
     )
