13e1fccebb56916146bfb6a82c59b2a8969b4d18
王義凱 <a73013110@gmail.com>
2022-09-14 21:55:07 +0800

    .webpackrc: 反向代理

--- code/myapp-dva/src/routes/Center.js
+++ code/myapp-dva/src/routes/Center.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react'
 import { withRouter } from 'dva/router'
+import request from '../utils/request'
 
 export default class Center extends Component {
+    componentDidMount() {
+        request("/ajax/comingList?ci=1&limit=10&movieIds=&token=&optimus_uuid=F6812C901C9C11EDABA1E3CF9445FF11557745DA670F42469F6CEA04C65E7E7D&optimus_risk_level=71&optimus_code=10").then(res => {
+            console.log(res)
+        })
+    }
+
     render() {
         return (
             <div>Center
