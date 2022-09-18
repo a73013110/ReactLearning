3faf95af13051233e78e5d8d0657967647f86fb9
王義凱 <a73013110@gmail.com>
2022-09-18 17:22:09 +0800

    UmiJS: 加入city

---
+++ code/myapp-umi/src/models/CityModel.ts
@@ -0,0 +1,7 @@
+export default {
+    namespace: "city",
+    state: {
+        cityName: "北京",
+        cityId: 110100
+    }
+}
--- code/myapp-umi/src/pages/Cinema.tsx
+++ code/myapp-umi/src/pages/Cinema.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { NavBar } from 'antd-mobile'
 import { SearchOutline } from 'antd-mobile-icons'
+import { useNavigate } from 'umi'
 
 export default function Cinema() {
+    const navigate = useNavigate()
+
     return (
         <div>
             <NavBar back="北京" backArrow={false} onBack={() => {
-                console.log("click")
+                navigate("/city")
             }} right={<SearchOutline />}>標題</NavBar>
             Cinema
         </div>
