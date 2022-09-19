f06444d3b4910d33221efd3a1fa4ae4e53c14f72
王義凱 <a73013110@gmail.com>
2022-09-19 22:37:37 +0800

    初次加入專案：React項目全球新聞發布管理系統 https://github.com/startbug/react-news-project

---
+++ code/newssystem/src/App.js
@@ -0,0 +1,14 @@
+import Child from './Child'
+import './App.css'
+function App(){
+  return <div>
+    app
+      <ul>
+        <li>11111</li>
+        <li>22222</li>
+      </ul>
+    <Child/>
+  </div>
+}
+
+export default App
