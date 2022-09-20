d737e555544dc19c9638b3ea186ef9bcb5f67b52
王義凱 <a73013110@gmail.com>
2022-09-20 22:19:14 +0800

    測試module.css

--- code/newssystem/src/App.tsx
+++ code/newssystem/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Child from './Child';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			app
+            <ul>
+                <li>1111</li>
+                <li>2222</li>
+            </ul>
+
+			<Child />
+		</div>
+	);
 }
 
 export default App;
