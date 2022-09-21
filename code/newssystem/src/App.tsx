f035724ff3debe64554811135dcbec27f1204fcd
王義凱 <a73013110@gmail.com>
2022-09-21 21:07:51 +0800

    撰寫路由前清理程式

--- code/newssystem/src/App.tsx
+++ code/newssystem/src/App.tsx
@@ -1,25 +1,7 @@
-import axios from 'axios';
-import { useEffect } from 'react';
-import './App.css'
-import Child from './Child';
-
 function App() {
-	useEffect(() => {
-		axios.get("/bilibiliApi/x/web-interface/nav").then(res => {
-			console.log(res)
-		})
-	}, [])
-
-
 	return (
 		<div>
 			app
-			<ul>
-				<li>1111</li>
-				<li>2222</li>
-			</ul>
-
-			<Child />
 		</div>
 	);
 }
