43779ab2160b697345bf1e07e432649b4a9c70b7
Yikai <a73013110@hanglong.com.tw>
2022-10-04 15:04:27 +0800

    完成首頁資料顯示

--- code/newssystem/src/pages/sandbox/home/Home.tsx
+++ code/newssystem/src/pages/sandbox/home/Home.tsx
@@ -1,24 +1,48 @@
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Card, Col, List, Row } from 'antd'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { INews } from '../../../interface/news/INews';
+import { useAppSelector } from '../../../redux/hooks';
 
 const { Meta } = Card;
 
 export default function Home() {
+    const userInfo = useAppSelector(state => state.auth.userInfo);
+    const [viewList, setViewList] = useState<INews[]>([]);
+    const [starList, setStarList] = useState<INews[]>([]);
+
+    useEffect(() => {
+        axios.get("/news?publishState=2&_expand=category&_sort=view&_order=desc&_limit=6").then(res => {
+            setViewList(res.data);
+        })
+    }, [])
+
+    useEffect(() => {
+        axios.get("/news?publishState=2&_expand=category&_sort=star&_order=desc&_limit=6").then(res => {
+            setStarList(res.data);
+        })
+    }, [])
+
     return (
         <div className="site-card-wrapper">
             <Row gutter={16}>
                 <Col span={8}>
-                    <Card title="用戶者最常瀏覽" bordered={true}>
+                    <Card title="用戶最常瀏覽" bordered={true}>
                         <List
                             size="small"
-                            dataSource={["111", "111"]}
-                            renderItem={item => <List.Item>{item}</List.Item>}
+                            dataSource={viewList}
+                            renderItem={item => <List.Item><a href={`#/news-manage/preview/${item.id}`}>{item.title}</a></List.Item>}
                         />
                     </Card>
                 </Col>
                 <Col span={8}>
-                    <Card title="Card title" bordered={true}>
-                        Card content
+                    <Card title="用戶點讚最多" bordered={true}>
+                        <List
+                            size="small"
+                            dataSource={starList}
+                            renderItem={item => <List.Item><a href={`#/news-manage/preview/${item.id}`}>{item.title}</a></List.Item>}
+                        />
                     </Card>
                 </Col>
                 <Col span={8}>
@@ -37,8 +61,11 @@ export default function Home() {
                     >
                         <Meta
                             avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-                            title="Card title"
-                            description="This is the description"
+                            title={userInfo.username}
+                            description={<div>
+                                <b>{userInfo.region ? userInfo.region : "全球"}</b>
+                                <span style={{ paddingLeft: "10px" }}>{userInfo.role.roleName}</span>
+                            </div>}
                         />
                     </Card>
                 </Col>
